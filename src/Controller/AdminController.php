b2d3fdf04eb4259c684bb278b4ef3553b40bb93f
Ttst <thibautstachnick@gmail.com>
2024-04-08 15:03:04 +0200

    add a controller for admin page

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,17 +2,44 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
+use App\Form\ProductType;
+use App\Repository\ProductRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route("/admin")]
 class AdminController extends AbstractController
 {
-    #[Route('/admin', name: 'app_admin')]
-    public function index(): Response
+    #[Route('/', name: 'app_admin')]
+    public function index(ProductRepository $repository): Response
     {
         return $this->render('admin/index.html.twig', [
-            'controller_name' => 'AdminController',
+            'products' => $repository->findAll(),
+        ]);
+    }
+
+
+    #[Route("/new",name: "app_admin_create")]
+    public function createProduct(EntityManagerInterface $manager, Request $request):Response{
+
+
+        $product = new Product();
+        $productForm = $this->createForm(ProductType::class,$product);
+        $productForm->handleRequest($request);
+        if ($productForm->isSubmitted() && $productForm->isValid()){
+
+            $manager->persist($product);
+            $manager->flush();
+            return $this->redirectToRoute("app_admin");
+        }
+
+        return $this->render("admin/new.html.twig",[
+            "image"=>null,
+            "productForm"=>$productForm->createView()
         ]);
     }
 }

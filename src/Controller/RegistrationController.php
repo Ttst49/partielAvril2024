e1aa46820bb3c87427792dc9dceb131b42fc1728
Ttst <thibautstachnick@gmail.com>
2024-04-09 09:14:51 +0200

    add custom register method with api

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -10,6 +10,8 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Serializer\SerializerInterface;
+
 
 class RegistrationController extends AbstractController
 {
@@ -40,5 +42,34 @@ class RegistrationController extends AbstractController
         return $this->render('registration/register.html.twig', [
             'registrationForm' => $form,
         ]);
+    }
+
+    #[Route('api/register', name: 'app_register_json')]
+    public function registerJson(SerializerInterface $serializer,Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $manager): Response
+    {
+
+        $user = $serializer->deserialize($request->getContent(), User::class, "json");
+
+        $parameters = json_decode($request->getContent(), true);
+
+        $user->setPassword(
+            $userPasswordHasher->hashPassword(
+                $user,
+                $parameters["password"]
+            )
+        );
+
+
+        $manager->persist($user);
+        $manager->flush();
+
+        $response = [
+            "content"=> "The user ".$user->getUsername()." has been created",
+            "status"=>201,
+            "user"=>$user
+        ];
+
+        return $this->json($response, 201, [], ["groups" => "forUserIndexing"]);
+
     }
 }
